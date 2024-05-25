b3a4e7f1ce62d2c1027cb817f9883aa29f64dd96
karppinski <krzysztof.karpinski135@gmail.com>
2024-05-25 19:42:28 +0200

    added models

---
+++ Domain/Entities/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; }
+        public Guid MechanicId { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime AppontmentDate { get; set; }
+    }
+}
---
+++ Domain/ValueObject/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObject
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
